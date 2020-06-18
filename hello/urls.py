4be16e288acc57ef707b394087bde7ea965bcc54
paul <brianwangombe11@gmail.com>
2020-06-19 02:59:11 +0300

    everything is working fine

---
+++ hello/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import Hello
+
+
+urlpatterns = [
+    path('', Hello.as_view(), name="hello")
+]
---
+++ hello/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class Hello(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        message = "Hello, World!"
+        return Response(message)
+
